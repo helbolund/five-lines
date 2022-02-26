b66c447557c4dc9285301ce57b79c5549aeeec3b
Morten Helbo Lund <mhlund@gmail.com>
2022-02-26 17:17:40 +0100

    EXTRACT METHOD

--- index.ts
+++ index.ts
@@ -431,12 +431,16 @@ function drawPlayer(g: CanvasRenderingContext2D) {
 function drawMap(g: CanvasRenderingContext2D) {
   for (let y = 0; y < map.length; y++) {
     for (let x = 0; x < map[y].length; x++) {
-      map[y][x].color(g);
-
-      if (!map[y][x].isAir() && !map[y][x].isPlayer())
-        g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+      drawTile(y, x, g);
     }
   }
+}
+
+function drawTile(y: number, x: number, g: CanvasRenderingContext2D) {
+  map[y][x].color(g);
+
+  if (!map[y][x].isAir() && !map[y][x].isPlayer())
+    g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
 }
 
 function gameLoop() {
