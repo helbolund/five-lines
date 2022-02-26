cc6670ba2e8b6ed1c95c8c1acd9009cc0be13040
Morten Helbo Lund <mhlund@gmail.com>
2022-02-26 16:24:52 +0100

    Fix map create

--- index.ts
+++ index.ts
@@ -14,7 +14,7 @@ enum RawTile {
   KEY2, LOCK2
 }
 
-interface Tile2 {
+interface Tile {
   isAir(): boolean;
   isFlux() : boolean;
   isUnbreakable() : boolean;
@@ -29,8 +29,8 @@ interface Tile2 {
   isLock2() : boolean;
 }
 
-class Air implements Tile2 {
-  isAir() { return false; };
+class Air implements Tile {
+  isAir() { return true; };
   isFlux()  { return false; };
   isUnbreakable()  { return false; };
   isPlayer()  { return false; };
@@ -44,9 +44,9 @@ class Air implements Tile2 {
   isLock2()  { return false; };
 }
 
-class Flux implements Tile2 {
+class Flux implements Tile {
   isAir() { return false; };
-  isFlux()  { return false; };
+  isFlux()  { return true; };
   isUnbreakable()  { return false; };
   isPlayer()  { return false; };
   isStone()  { return false; };
@@ -59,10 +59,10 @@ class Flux implements Tile2 {
   isLock2()  { return false; };
 }
 
-class Unbreakable implements Tile2 {
+class Unbreakable implements Tile {
   isAir() { return false; };
   isFlux()  { return false; };
-  isUnbreakable()  { return false; };
+  isUnbreakable()  { return true; };
   isPlayer()  { return false; };
   isStone()  { return false; };
   isFallingStone()  { return false; };
@@ -74,11 +74,11 @@ class Unbreakable implements Tile2 {
   isLock2()  { return false; };
 }
 
-class Player implements Tile2 {
+class Player implements Tile {
   isAir() { return false; };
   isFlux()  { return false; };
   isUnbreakable()  { return false; };
-  isPlayer()  { return false; };
+  isPlayer()  { return true; };
   isStone()  { return false; };
   isFallingStone()  { return false; };
   isBox()  { return false; };
@@ -89,12 +89,12 @@ class Player implements Tile2 {
   isLock2()  { return false; };
 }
 
-class Stone implements Tile2 {
+class Stone implements Tile {
   isAir() { return false; };
   isFlux()  { return false; };
   isUnbreakable()  { return false; };
   isPlayer()  { return false; };
-  isStone()  { return false; };
+  isStone()  { return true; };
   isFallingStone()  { return false; };
   isBox()  { return false; };
   isFallingBox()  { return false; };
@@ -104,13 +104,13 @@ class Stone implements Tile2 {
   isLock2()  { return false; };
 }
 
-class FallingStone implements Tile2 {
+class FallingStone implements Tile {
   isAir() { return false; };
   isFlux()  { return false; };
   isUnbreakable()  { return false; };
   isPlayer()  { return false; };
   isStone()  { return false; };
-  isFallingStone()  { return false; };
+  isFallingStone()  { return true; };
   isBox()  { return false; };
   isFallingBox()  { return false; };
   isKey1()  { return false; };
@@ -119,14 +119,14 @@ class FallingStone implements Tile2 {
   isLock2()  { return false; };
 }
 
-class Box implements Tile2 {
+class Box implements Tile {
   isAir() { return false; };
   isFlux()  { return false; };
   isUnbreakable()  { return false; };
   isPlayer()  { return false; };
   isStone()  { return false; };
   isFallingStone()  { return false; };
-  isBox()  { return false; };
+  isBox()  { return true; };
   isFallingBox()  { return false; };
   isKey1()  { return false; };
   isLock1()  { return false; };
@@ -134,7 +134,7 @@ class Box implements Tile2 {
   isLock2()  { return false; };
 }
 
-class FallingBox implements Tile2 {
+class FallingBox implements Tile {
   isAir() { return false; };
   isFlux()  { return false; };
   isUnbreakable()  { return false; };
@@ -142,14 +142,14 @@ class FallingBox implements Tile2 {
   isStone()  { return false; };
   isFallingStone()  { return false; };
   isBox()  { return false; };
-  isFallingBox()  { return false; };
+  isFallingBox()  { return true; };
   isKey1()  { return false; };
   isLock1()  { return false; };
   isKey2()  { return false; };
   isLock2()  { return false; };
 }
 
-class Key1 implements Tile2 {
+class Key1 implements Tile {
   isAir() { return false; };
   isFlux()  { return false; };
   isUnbreakable()  { return false; };
@@ -158,13 +158,13 @@ class Key1 implements Tile2 {
   isFallingStone()  { return false; };
   isBox()  { return false; };
   isFallingBox()  { return false; };
-  isKey1()  { return false; };
+  isKey1()  { return true; };
   isLock1()  { return false; };
   isKey2()  { return false; };
   isLock2()  { return false; };
 }
 
-class Lock1 implements Tile2 {
+class Lock1 implements Tile {
   isAir() { return false; };
   isFlux()  { return false; };
   isUnbreakable()  { return false; };
@@ -174,12 +174,12 @@ class Lock1 implements Tile2 {
   isBox()  { return false; };
   isFallingBox()  { return false; };
   isKey1()  { return false; };
-  isLock1()  { return false; };
+  isLock1()  { return true; };
   isKey2()  { return false; };
   isLock2()  { return false; };
 }
 
-class Key2 implements Tile2 {
+class Key2 implements Tile {
   isAir() { return false; };
   isFlux()  { return false; };
   isUnbreakable()  { return false; };
@@ -190,11 +190,11 @@ class Key2 implements Tile2 {
   isFallingBox()  { return false; };
   isKey1()  { return false; };
   isLock1()  { return false; };
-  isKey2()  { return false; };
+  isKey2()  { return true; };
   isLock2()  { return false; };
 }
 
-class Lock2 implements Tile2 {
+class Lock2 implements Tile {
   isAir() { return false; };
   isFlux()  { return false; };
   isUnbreakable()  { return false; };
@@ -206,7 +206,7 @@ class Lock2 implements Tile2 {
   isKey1()  { return false; };
   isLock1()  { return false; };
   isKey2()  { return false; };
-  isLock2()  { return false; };
+  isLock2()  { return true; };
 }
 
 interface Input {
@@ -236,7 +236,7 @@ class Down implements Input {
 
 let playerx = 1;
 let playery = 1;
-let map: Tile[][] = [
+let rawMap: RawTile[][] = [
   [2, 2, 2, 2, 2, 2, 2, 2],
   [2, 3, 0, 1, 1, 2, 0, 2],
   [2, 4, 2, 6, 1, 2, 0, 2],
@@ -244,6 +244,38 @@ let map: Tile[][] = [
   [2, 4, 1, 1, 1, 9, 0, 2],
   [2, 2, 2, 2, 2, 2, 2, 2],
 ];
+let map: Tile[][];
+function assertExhausted(x: never): never {
+  throw new Error("Unexpected object: " + x);
+}
+
+function transformTile(tile: RawTile) {
+  switch (tile) {
+    case RawTile.AIR: return new Air();
+    case RawTile.FLUX: return new Flux();
+    case RawTile.UNBREAKABLE: return new Unbreakable();
+    case RawTile.PLAYER: return new Player();
+    case RawTile.STONE: return new Stone();
+    case RawTile.FALLING_STONE: return new FallingStone();
+    case RawTile.BOX: return new Box();
+    case RawTile.FALLING_BOX: return new FallingBox();
+    case RawTile.KEY1: return new Key1();
+    case RawTile.LOCK1: return new Lock1();
+    case RawTile.KEY2: return new Key2();
+    case RawTile.LOCK2: return new Lock2();
+    default: assertExhausted(tile);
+  }
+}
+
+function transformMap() {
+  map = new Array(rawMap.length);
+  for (let y = 0; y < rawMap.length; y++) {
+    map[y] = new Array(rawMap[y].length);
+    for (let x = 0; x < rawMap[y].length; x++) {
+      map[y][x] = transformTile(rawMap[y][x]);
+    }
+  }
+}
 
 let inputs: Input[] = [];
 
@@ -398,6 +430,7 @@ function gameLoop() {
 }
 
 window.onload = () => {
+  transformMap();
   gameLoop();
 }
 
