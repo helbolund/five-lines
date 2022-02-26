c14d2426d1ecd2e178fad79f03b27b60bf87988e
Morten Helbo Lund <mhlund@gmail.com>
2022-02-26 17:39:17 +0100

    BUG: updateTile made falling stone into box

--- index.ts
+++ index.ts
@@ -389,7 +389,7 @@ function updateMap() {
 function updateTile(y: number, x: number) {
   if ((map[y][x].isStone() || map[y][x].isFallingStone())
     && map[y + 1][x].isAir()) {
-    map[y + 1][x] = new FallingBox();
+    map[y + 1][x] = new FallingStone();
     map[y][x] = new Air();
   } else if ((map[y][x].isBox() || map[y][x].isFallingBox())
     && map[y + 1][x].isAir()) {
